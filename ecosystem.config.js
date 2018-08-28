ddef7cf37370a6bd6b6e339ba0661b5a9f80b243
Derk Weijers <derk@derkweijers.nl>
2018-08-28 13:37:52 +0200

    Checkout test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
     apps : [{
         name      : 'SNAPI website',
         script    : 'npm',
-        args: 'run start:production',
         env_production : {
-            NODE_ENV: 'production'
+            NODE_ENV: 'production',
+            args: 'run start:production'
         },
         env_test : {
-            NODE_ENV: 'test'
+            NODE_ENV: 'test',
+            args: 'run start:test'
         }
     }],
 
@@ -23,10 +24,10 @@ module.exports = {
         test : {
             user : 'derk',
             host : '142.93.226.193',
-            ref  : 'origin/master',
+            ref  : 'origin/test',
             repo : 'git@github.com:derkweijers/snapiwebsite.git',
             path : '/var/www/snapiwebsite-test',
-            'post-deploy' : 'git checkout test && npm install && pm2 reload ecosystem.config.js --env production'
+            'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
         }
     }
 };
