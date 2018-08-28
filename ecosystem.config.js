499dc9bb5858e7ed4eff77f2b672bccc6e4b0209
Derk Weijers <derk@derkweijers.nl>
2018-08-28 13:29:08 +0200

    Updated pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
         args: 'run start:production',
         env_production : {
             NODE_ENV: 'production'
+        },
+        env_test : {
+            NODE_ENV: 'test'
         }
     }],
 
@@ -13,9 +16,17 @@ module.exports = {
             user : 'derk',
             host : '142.93.226.193',
             ref  : 'origin/master',
-            repo : 'git@github.com:repo.git',
+            repo : 'git@github.com:derkweijers/snapiwebsite.git',
             path : '/var/www/snapiwebsite-prod',
             'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+        },
+        test : {
+            user : 'derk',
+            host : '142.93.226.193',
+            ref  : 'origin/master',
+            repo : 'git@github.com:derkweijers/snapiwebsite.git',
+            path : '/var/www/snapiwebsite-test',
+            'post-deploy' : 'git checkout test && npm install && pm2 reload ecosystem.config.js --env production'
         }
     }
 };
