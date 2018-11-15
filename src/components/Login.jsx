b0e35187a410672bccea887f2bd201b55d292eb9
Derk Weijers <derk@derkweijers.nl>
2018-11-15 10:09:40 +0100

    [WIP] Deleted auth0 work, starting again.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import Auth from "../auth/Auth";
 
 class Login extends Component {
   render() {
-    const auth = new Auth();
 
     return (
       <div>
-        {auth.login()}
       </div>
     );
   }
