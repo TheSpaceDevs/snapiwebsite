a5a1e0322832d236fee93de5d279a4454228ac5d
Derk Weijers <derk@derkweijers.nl>
2018-11-23 10:30:29 +0100

    Dynamic callback URL loading

--- src/config/callback.js
+++ src/config/callback.js
@@ -3,10 +3,12 @@ const hostname = window && window.location && window.location.hostname;
 
 if(hostname === 'localhost') {
   backendHost = 'http://localhost:3000';
-} else if(hostname === 'testsite.spaceflightnewsapi.net') {
-  backendHost = 'https://testsite.spaceflightnewsapi.net';
+} else if(hostname === 'test.spaceflightnewsapi.net') {
+  backendHost = 'https://test.spaceflightnewsapi.net';
 } else if(hostname === 'www.spaceflightnewsapi.net') {
   backendHost = 'https://www.spaceflightnewsapi.net';
+} else {
+  backendHost = 'Wrong host'
 }
 
 export const CALLBACK = `${backendHost}/callback`;
