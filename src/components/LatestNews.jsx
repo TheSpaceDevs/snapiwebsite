ca2a68942249890e43f0349da2d218e82325fc7d
Derk Weijers <derk@derkweijers.nl>
2019-03-09 20:19:01 +0100

    Show the 5 latest articles on the homepage

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
-// import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col } from "reactstrap";
-import LatestNewsCard from "./LatestNewsCard";
+import { Col, Row, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from "reactstrap";
+import axios from "axios";
 
 
 class LatestNews extends Component {
+  state = {
+    news: []
+  };
+
+  async componentWillMount() {
+    let news = await axios.get("https://test.spaceflightnewsapi.net/api/v1/articles?limit=5");
+    this.setState({
+      news: news.data.docs
+    });
+  }
+
   render() {
+    const listItem = this.state.news.map(article => {
+      return (
+        <ListGroupItem key={article._id} tag="a" href={article.url}>
+          <ListGroupItemHeading>{article.title}</ListGroupItemHeading>
+          <ListGroupItemText>{article.news_site_long}</ListGroupItemText>
+        </ListGroupItem>
+      );
+    });
+
     return (
       <div style={{ textAlign: "center" }}>
-        <h1>Latest News</h1>
-        <LatestNewsCard/>
+        <h1>Latest</h1>
+        <Row>
+          <Col xs={12} sm={6}>
+            <ListGroup>
+              {listItem}
+            </ListGroup>
+          </Col>
+        </Row>
       </div>
     );
   }
