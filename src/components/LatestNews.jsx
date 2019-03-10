4bc1efb9e0df51093d8ff998261b6f099f74d6a6
Derk Weijers <derk@derkweijers.nl>
2019-03-10 17:44:01 +0100

    Also show latest blogs.

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 
 class LatestNews extends Component {
   state = {
-    news: []
+    news: [],
+    blogs: []
   };
 
   async componentWillMount() {
@@ -13,14 +14,28 @@ class LatestNews extends Component {
     this.setState({
       news: news.data.docs
     });
+
+    let blogs = await axios.get("https://test.spaceflightnewsapi.net/api/v1/blogs?limit=5");
+    this.setState({
+      blogs: blogs.data.docs
+    });
   }
 
   render() {
-    const listItem = this.state.news.map(article => {
+    const listArticles = this.state.news.map(article => {
       return (
-        <ListGroupItem key={article._id} tag="a" href={article.url}>
+        <ListGroupItem style={{ color: "#2C3E50" }} key={article._id} tag="a" href={article.url}>
           <ListGroupItemHeading>{article.title}</ListGroupItemHeading>
           <ListGroupItemText>{article.news_site_long}</ListGroupItemText>
+        </ListGroupItem>
+      );
+    });
+
+    const listBlogs = this.state.blogs.map(blog => {
+      return (
+        <ListGroupItem style={{ color: "#2C3E50" }} key={blog._id} tag="a" href={blog.url}>
+          <ListGroupItemHeading>{blog.title}</ListGroupItemHeading>
+          <ListGroupItemText>{blog.news_site_long}</ListGroupItemText>
         </ListGroupItem>
       );
     });
@@ -30,8 +45,15 @@ class LatestNews extends Component {
         <h1>Latest</h1>
         <Row>
           <Col xs={12} sm={6}>
-            <ListGroup>
-              {listItem}
+            <h2>Articles</h2>
+            <ListGroup flush>
+              {listArticles}
+            </ListGroup>
+          </Col>
+          <Col xs={12} sm={6}>
+            <h2>Blogs</h2>
+            <ListGroup flush>
+              {listBlogs}
             </ListGroup>
           </Col>
         </Row>
