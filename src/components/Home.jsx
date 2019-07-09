0e0f96ff7100b9e71f1550fd1fa50a5ad11b77af
Derk Weijers <derk@derkweijers.nl>
2019-07-09 15:34:57 +0200

    Added buttons to platforms

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,8 +25,12 @@ class Home extends Component {
             <Button onClick={() => window.open(`${this.selectUrl()}/api/v1`)}
                     color="primary">Go To Docs</Button>
           </p>
-          <a href="https://www.patreon.com/bePatron?u=8565105" data-patreon-widget-type="become-patron-button">Become a Patron!</a>
-        </Jumbotron>
+            <a href='https://www.patreon.com/bePatron?u=8565105' target="_blank" rel="noopener noreferrer"><img height={35} alt='Become a Patron!' src='https://c5.patreon.com/external/logo/become_a_patron_button.png'/></a>
+          <div>
+            <a href='https://apps.apple.com/us/app/spaceflight-news/id1438543016?l=nl&ls=1' target="_blank" rel="noopener noreferrer"><img height={35} width={105} style={{ margin: 5, marginTop: 10}} alt='Download from the App Store' src={require('./assets/appstore_download.svg')}/></a>
+            <a href='https://play.google.com/store/apps/details?id=nl.derkweijers.spaceflightnews&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' target="_blank" rel="noopener noreferrer"><img height={35} width={105} style={{ margin: 5, marginTop: 10}} alt='Get it on Google Play' src={require('./assets/google-play-badge.png')}/></a>
+          </div>
+          </Jumbotron>
         <Container>
           <About/>
           <LatestNews/>
