2e88effd945133a8ecaf3e8565b27c94d62d2fe3
Derk Weijers <derk@weijers.xyz>
2019-11-13 13:02:55 +0100

    More content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import { createBrowserHistory } from "history";
+
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Apps from "./components/Apps";
+
+const appHistory = createBrowserHistory();
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-    </div>
-  );
+    return (
+        <Router history={appHistory}>
+            <NavBar/>
+            <Switch>
+                <Route path="/apps">
+                    <Apps />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import {Button, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const NavBar = () => {
     return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/home">Spaceflight News API</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/apps">Apps</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
                 </Navbar.Collapse>
             </Navbar>
     );
