0a642f00f40aff1a65dd6b2d3c64839c5f540fe5
Derk Weijers <derk@weijers.xyz>
2019-12-14 13:53:55 +0100

    Responsive news cards

--- src/components/cards/NewsCard.js
+++ src/components/cards/NewsCard.js
@@ -5,28 +5,14 @@ function NewsCard(props) {
   const {title, site, url, date, image} = props;
 
   return (
-    <Card style={styles.card} className='shadow'>
+    <Card style={{height: "100%"}} className='shadow'>
       <Card.Img variant="top" src={image} style={{height: "15rem"}}/>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title>{<a href={url}>{title}</a>}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{`${site} | ${new Date(date).toLocaleDateString()}`}</Card.Subtitle>
-        <div style={styles.linkStyle}>
-          <Card.Link href={url} target='_blank noopener noreferrer'>Read</Card.Link>
-        </div>
       </Card.Body>
     </Card>
   );
 }
-
-const styles = {
-  card: {
-    height: "27rem"
-  },
-  linkStyle: {
-    position: 'absolute',
-    bottom: 0,
-    marginBottom: '5px'
-  }
-};
 
 export default NewsCard;
