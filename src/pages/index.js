afcbb4e4d8f9999da10aebe1f15ac73df8e06670
Derk Weijers <derkweijers@users.noreply.github.com>
2020-11-10 09:19:50 +0100

    V2 (#40)
    * SNAPI v2 website
    
    * Wait till the news is loaded
    
    * Different grouping
    
    * favicon and logo updated
    
    * New feature images
    
    * No news page for now
    
    * Showcase page done
    
    * Build command now goes to the snapi-backend public folder
    
    * Updated footer, images, and text
    
    * Make the urls environment aware
    
    * Added more apps to the showcase page
    
    * Cards should be max height
    
    * updates
    
    * Small update to links
    
    * Final v2 updates

---
+++ src/pages/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const url = `https://www.spaceflightnewsapi.net`
+
+const features = [
+  {
+    title: <>News</>,
+    imageUrl: 'img/features/News.svg',
+    description: (
+      <>
+        Get an overview of the latest Spaceflight news, from various sources! Easily link your users to the right websites
+      </>
+    ),
+  },
+  {
+    title: <>Blogs</>,
+    imageUrl: 'img/features/Blogs.svg',
+    description: (
+      <>
+        Blogs often provide a more detailed overview of launches and missions. A must-have for the serious spaceflight enthusiast
+      </>
+    ),
+  },
+  {
+    title: <>Reports</>,
+    imageUrl: 'img/features/Reports.svg',
+    description: (
+      <>
+        Space stations and other missions often publish their data. With SNAPI, you can include it in your app as well!
+      </>
+    ),
+  },
+  {
+    title: <>Launch Library 2 Integration</>,
+    imageUrl: 'img/features/LL2.svg',
+    description: (
+      <>
+        Already using Launch Library 2 to get launches and events? Awesome! SNAPI offers integration so you can build relations between news and launches/events
+      </>
+    ),
+  }
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--6', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title="Home"
+      description="The Spaceflight News API lets you add space related news to your apps.">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl(`${url}/documentation`)}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
---
+++ src/pages/showcase.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import styles from './styles.module.css'
+import apps from '../data/apps.json'
+
+const Showcase = () => {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+
+  const Apps = () => {
+    return apps.map(app => {
+      return (
+        <div key={app.name} className={clsx("col col--3", styles.appCol)}>
+          <div className={clsx("card", styles.appCard)}>
+            <div className="card__header">
+              <div className="avatar">
+                <img
+                  className="avatar__photo"
+                  src={app.image}
+                />
+                <div className="avatar__intro">
+                  <h4 className="avatar__name">{app.name}</h4>
+                  <small className="avatar__subtitle">
+                    {
+                      app.platforms.map(platform => {
+                        return <span className="badge badge--primary" style={{marginLeft: 1, marginRight: 1}}>{platform}</span>
+                      })
+                    }
+                  </small>
+                </div>
+              </div>
+            </div>
+            <div className="card__footer">
+                <a href={app.website} className="button button--secondary">Website</a>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  return (
+    <Layout
+      title="Showcase"
+      description="These apps and services are using the Spaceflight News API">
+      <div className="container">
+        <div className="row">
+          <Apps/>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Showcase;
